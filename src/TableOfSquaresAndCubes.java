300bbc39387589586a6e43a57618534b7ee36be7
clreaume <caralreaume@gmail.com>
2018-01-24 16:58:51 -0500

    initial commit of Lab 3

---
+++ src/TableOfSquaresAndCubes.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TableOfSquaresAndCubes {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String ans;
+
+		do {
+			System.out.println("Please enter an integer: ");
+			int num = input.nextInt();
+
+			System.out.printf("%15s %15s %15s\n", "Number", "Squared", "Cubed");
+			System.out.printf("%15s %15s %15s\n", "======", "======", "======");
+
+			for (int i = 1; i <= num; ++i) {
+				System.out.printf("%15d %15d %15d\n", i, i * i, i * i * i);
+			}
+
+			System.out.println("Want to keep going? Enter Y if so: ");
+			ans = input.next();
+
+		} while (ans.equalsIgnoreCase("Y"));
+		
+		System.out.println("K, bye!");
+		
+		input.close();
+	}
+}
