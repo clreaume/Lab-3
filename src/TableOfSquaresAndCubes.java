8b0eacc73fc4aa6c54196ab27ab179659a17c458
clreaume <caralreaume@gmail.com>
2018-01-24 21:28:55 -0500

    now uses Math.pow

--- src/TableOfSquaresAndCubes.java
+++ src/TableOfSquaresAndCubes.java
@@ -13,16 +13,16 @@ public class TableOfSquaresAndCubes {
 			System.out.printf("%15s %15s %15s\n", "======", "======", "======");
 
 			for (int i = 1; i <= num; ++i) {
-				System.out.printf("%15d %15d %15d\n", i, i * i, i * i * i);
+				System.out.printf("%15d %15d %15d\n", i, (int)Math.pow(i, 2), (int)Math.pow(i, 3));
 			}
 
 			System.out.println("Want to keep going? Enter Y if so: ");
 			ans = input.next();
 
 		} while (ans.equalsIgnoreCase("Y"));
-		
+
 		System.out.println("K, bye!");
-		
+
 		input.close();
 	}
 }
